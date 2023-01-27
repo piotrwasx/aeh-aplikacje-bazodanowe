ca3e97f420b0f119e6e131055d25db3b8a980539
piotrwas <ns.piotr@gmail.com>
2023-01-27 22:33:27 +0100

    Add: ClientsRentingController

---
+++ aeh_aplikacje_bazodanowe/Controllers/ClientsRentingController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using aeh_aplikacje_bazodanowe.Models;
+
+namespace aeh_aplikacje_bazodanowe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsRentingController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public ClientsRentingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet("byClient")]
+        public JsonResult Get(int client_id)
+        {
+            string query = @"exec allRentsFromClientBy @id = @client_id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@client_id", client_id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+    }
+}
