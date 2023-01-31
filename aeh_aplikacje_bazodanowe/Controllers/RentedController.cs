ff07d5407e7bed015bca31d2a68279fd8c438410
piotrwas <ns.piotr@gmail.com>
2023-01-31 19:41:51 +0100

    minor fixes

--- aeh_aplikacje_bazodanowe/Controllers/RentedController.cs
+++ aeh_aplikacje_bazodanowe/Controllers/RentedController.cs
@@ -13,25 +13,25 @@ namespace aeh_aplikacje_bazodanowe.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CarsController : ControllerBase
+    public class RentedController : ControllerBase
     {
 
         private readonly IConfiguration _configuration;
-        public CarsController(IConfiguration configuration)
+        public RentedController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
 
-        [HttpGet]
-        public JsonResult Get()
+        [HttpGet("cars")]
+        public JsonResult GetCars()
         {
             string query = @"
                             SELECT id,
                             car_brand,
                             car_model
                             FROM
-                            dbo.Cars
+                            dbo.Rented_Cars
                             ";
 
             DataTable table = new DataTable();
@@ -52,7 +52,65 @@ namespace aeh_aplikacje_bazodanowe.Controllers
             return new JsonResult(table);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("motorcycles")]
+        public JsonResult GetMotorcycles()
+        {
+            string query = @"
+                            SELECT id,
+                            motorcycle_brand,
+                            motorcycle_model
+                            FROM
+                            dbo.Rented_Motorcycles
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("utilities")]
+        public JsonResult GetUtilities()
+        {
+            string query = @"
+                            SELECT id,
+                            utility_brand,
+                            utility_model
+                            FROM
+                            dbo.Rented_Utilities
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet("car")]
         public JsonResult Get(int id)
         {
             string query = @"
@@ -61,12 +119,12 @@ namespace aeh_aplikacje_bazodanowe.Controllers
                             car_model,
                             car_year,
                             car_mileage_km,
-                            car_transmission,
+                            car_transmisson,
                             car_motor,
                             car_body_type,
                             car_rent_price_pln
                             FROM
-                            dbo.Cars
+                            dbo.Rented_Cars
                             WHERE
                             id = @car_id
                             ";
@@ -91,10 +149,10 @@ namespace aeh_aplikacje_bazodanowe.Controllers
             return new JsonResult(table);
         }
 
-        [HttpPost]
-        public JsonResult Post([FromBody]Car car)
+        [HttpPost("cars")]
+        public JsonResult Post([FromBody] Car car)
         {
-            string query = @"INSERT INTO dbo.Cars
+            string query = @"INSERT INTO dbo.Rented_Cars
                              (car_brand, car_model, car_year,
                              car_mileage_km, car_transmisson, car_motor,
                              car_body_type, car_rent_price_pln)
@@ -129,79 +187,7 @@ namespace aeh_aplikacje_bazodanowe.Controllers
             return new JsonResult("Added Successfully");
         }
 
-        [HttpDelete("{id}")]
-        public JsonResult Delete(int id)
-        {
-            string query = @"
-                           DELETE FROM dbo.Cars
-                           WHERE id = @car_id
-                            ";
-
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("AppCon");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myCommand.Parameters.AddWithValue("@car_id", id);
-
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-
-            return new JsonResult("Deleted Successfully");
-        }
-
-        [HttpPut]
-        public JsonResult Put(Car car)
-        {
-            string query = @"
-                           UPDATE dbo.Cars
-                           SET
-                            car_brand = @car_brand,
-                            car_model = @car_model,
-                            car_year = @car_year,
-                            car_mileage_km = @car_mileage_km,
-                            car_transmisson = @car_transmisson,
-                            car_motor = @car_motor,
-                            car_body_type = @car_body_type,
-                            car_rent_price_pln = @car_rent_price_pln
-                           WHERE
-                            id = @car_id
-                            ";
-
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("AppCon");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myCommand.Parameters.AddWithValue("@car_id", car.id);
-                    myCommand.Parameters.AddWithValue("@car_brand", car.car_brand);
-                    myCommand.Parameters.AddWithValue("@car_model", car.car_model);
-                    myCommand.Parameters.AddWithValue("@car_year", car.car_year);
-                    myCommand.Parameters.AddWithValue("@car_mileage_km", car.car_mileage_km);
-                    myCommand.Parameters.AddWithValue("@car_transmisson", car.car_transmisson);
-                    myCommand.Parameters.AddWithValue("@car_motor", car.car_motor);
-                    myCommand.Parameters.AddWithValue("@car_body_type", car.car_body_type);
-                    myCommand.Parameters.AddWithValue("@car_rent_price_pln", car.car_rent_price_pln);
-
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-
-            return new JsonResult("Updated Successfully");
-        }
+        
 
     }
 }
